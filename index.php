1db794fade628c7eb67c84cc557cfb1ee35d03b2
Carlos Oliveira <carloseduardodalazenoliveira@gmail.com>
2025-05-14 21:12:02 -0300

    Modelo Base da Pagina Inicial Adicionado

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>FilmesFlix</title>
+    <link rel="shortcut icon" href="img/Logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="/styles.css">
+</head>
+<body>
+
+<header>
+  <div class="container">
+    <h1 class="TituloNav">FilmesFlix</h1>
+    <nav>
+      <a href="filmes.html">Filmes</a>
+      <a href="series.html">Séries</a>
+    </nav>
+  </div>
+</header>
+
+<main>
+    <section>
+        <h2>Destaques</h2>
+        <h3>Filmes:</h3>
+        <div class="recomendados">
+            <!--aq vai o carrosel-->
+        </div>
+            <h3>Series:</h3>
+        <div class="recomendados">
+            <!--aq vai o carrosel-->
+        </div>
+    </section>
+    <section>
+        <h2>Lançamentos</h2>
+        <ul>
+            <!--Ultimos 5 lançados -->
+        </ul>
+    </section>
+
+    <section>
+        <h2>Ganhadores do Oscar</h2>
+        <ul>
+            <!--o titulo ja fala burro -->
+        </ul>
+    </section>
+</main>
+
+<footer>
+  <div class="footer-container">
+    <div class="footer-logo"> 
+      <h2>FilmesFlix</h2>
+    </div>
+        
+    <p class="footer-copy">© 2025 FilmesFlix. Todos os direitos reservados.</p>
+  </div>
+</footer>
+
+
+</body>
+</html>
