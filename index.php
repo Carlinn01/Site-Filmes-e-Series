6f6bf1d494abe4e8cd7516ce76bef26584ae5ab8
Carlos Oliveira <carloseduardodalazenoliveira@gmail.com>
2025-05-19 14:43:49 -0300

    Pagina inicial atualizada

--- index.php
+++ index.php
@@ -23,63 +23,115 @@
 </header>
 
 <main>
-    <section class="destaques-section">
-      <h2>Destaques</h2>
-      <h3>Filmes:</h3>
-      <div class="carousel-container">
-        <button class="prev-btn">&#10094;</button>
-        <div class="carousel">
-          <div class="card">
-            <img src="/img/Premonição6.jpg" alt="Premonição 6" />
-            <h3>Premonição 6: Laços de Sangue</h3>
-          </div>
-          <div class="card">
-            <img src="/img/Manas.jpg" alt="Manas" />
-            <h3>Manas</h3>
-          </div>
-          <div class="card">
-            <img src="/img/Final Destination.jpg" alt="Final Destination: Bloodlines" />
-            <h3>Final Destination: Bloodlines</h3>
-          </div>
-          <div class="card">
-            <img src="/img/Homem com H.jpg" alt="Homem com H" />
-            <h3>Homem com H</h3>
-          </div>
-          <div class="card">
-            <img src="/img/Os enforncados.jpg" alt="Os Enforcados" />
-            <h3>Os Enforcados</h3>
-          </div>
-        </div>
-        <button class="next-btn">&#10095;</button>
+  <section class="destaques-section">
+  <h2>Destaques</h2>
+  <h3>Filmes:</h3>
+  <div class="carousel-container">
+    <button class="prev-btn">&#10094;</button>
+    <div class="carousel">
+
+      <div class="card">
+        <img src="https://upload.wikimedia.org/wikipedia/pt/b/b3/Inside_Out_2.png" alt="Inside Out 2" />
+        <h3>Inside Out 2</h3>
       </div>
-      <h3>Series:</h3>
-      <div class="carousel-container">
-        <button class="prev-btn">&#10094;</button>
-        <div class="carousel">
-          <div class="card">
-            <img src="/img/Premonição6.jpg" alt="Premonição 6" />
-            <h3>Premonição 6: Laços de Sangue</h3>
-          </div>
-          <div class="card">
-            <img src="/img/Manas.jpg" alt="Manas" />
-            <h3>Manas</h3>
-          </div>
-          <div class="card">
-            <img src="/img/Final Destination.jpg" alt="Final Destination: Bloodlines" />
-            <h3>Final Destination: Bloodlines</h3>
-          </div>
-          <div class="card">
-            <img src="/img/Homem com H.jpg" alt="Homem com H" />
-            <h3>Homem com H</h3>
-          </div>
-          <div class="card">
-            <img src="/img/Os enforncados.jpg" alt="Os Enforcados" />
-            <h3>Os Enforcados</h3>
-          </div>
-        </div>
-        <button class="next-btn">&#10095;</button>
+
+      <div class="card">
+        <img src="https://m.media-amazon.com/images/S/pv-target-images/dd6fb66997dd4cb5606b587bb255d2cd2cec20ecbd11852a8f6b07a1358d09a1.jpg" alt="Deadpool & Wolverine" />
+        <h3>Deadpool & Wolverine</h3>
       </div>
-    </section>
+
+      <div class="card">
+        <img src="https://upload.wikimedia.org/wikipedia/pt/7/73/Moana_2_poster.jpg" alt="Moana 2" />
+        <h3>Moana 2</h3>
+      </div>
+
+      <div class="card">
+        <img src="https://ingresso-a.akamaihd.net/prd/img/movie/meu-malvado-favorito-4/4d756249-3b88-4b59-baa2-527f5e3d4c3e.webp" alt="Despicable Me 4" />
+        <h3>Meu Malvado Favorito 4</h3>
+      </div>
+
+      <div class="card">
+        <img src="https://www.universalpics.com.br/tl_files/content/movies/wicked/posters/01.jpg" alt="Wicked" />
+        <h3>Wicked</h3>
+      </div>
+
+      <div class="card">
+        <img src="https://m.media-amazon.com/images/M/MV5BNzk5MTE4YTUtNGU2My00MTYxLWE5NGItODk4YWFkOWYyMjA5XkEyXkFqcGc@._V1_.jpg" alt="Dune: Part Two" />
+        <h3>Duna: Parte 2</h3>
+      </div>
+
+      <div class="card">
+        <img src="https://lumiere-a.akamaihd.net/v1/images/7e31d6246c6f32fbb227b4739fb5440f_da23af1f.jpeg" alt="Mufasa: The Lion King" />
+        <h3>Mufasa: O Rei Leão</h3>
+      </div>
+
+      <div class="card">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0Bmm1_i_4vAcv-IYHVdhpV6T7s6OU-NVXpA&s" alt="Godzilla x Kong: The New Empire" />
+        <h3>Godzilla e Kong: O Novo Império</h3>
+      </div>
+
+      <div class="card">
+        <img src="https://upload.wikimedia.org/wikipedia/pt/7/7f/Kung_Fu_Panda_4_poster.jpg" alt="Kung Fu Panda 4" />
+        <h3>Kung Fu Panda 4</h3>
+      </div>
+
+      <div class="card">
+        <img src="https://m.media-amazon.com/images/S/pv-target-images/d3d170cab1ef757a9a437cb9aa70bb33a8897f31f327f02c2de4d5a50dd32ff4.jpg" alt="Sonic the Hedgehog 3" />
+        <h3>Sonic 3</h3>
+      </div>
+
+    </div>
+    <button class="next-btn">&#10095;</button>
+  </div>
+  <!-- séries -->
+  <h3>Séries:</h3>
+  <div class="carousel-container">
+    <button class="prev-btn">&#10094;</button>
+    <div class="carousel">
+
+      <div class="card">
+        <img src="https://br.web.img3.acsta.net/c_310_420/pictures/22/11/30/19/53/5856320.jpg" alt="The Last Of Us" />
+        <h3>The Last Of Us</h3>
+      </div>
+
+      <div class="card">
+        <img src="https://br.web.img2.acsta.net/c_310_420/pictures/19/03/21/16/15/4239577.jpg" alt="Game of Thrones" />
+        <h3>Game of Thrones</h3>
+      </div>
+
+      <div class="card">
+        <img src="https://br.web.img3.acsta.net/c_310_420/pictures/18/09/10/15/10/1186940.jpg" alt="A Descoberta das Bruxas" />
+        <h3>A Descoberta das Bruxas</h3>
+      </div>
+
+      <div class="card">
+        <img src="https://br.web.img3.acsta.net/c_310_420/pictures/14/03/31/19/28/462555.jpg" alt="Breaking Bad" />
+        <h3>Breaking Bad</h3>
+      </div>
+
+      <div class="card">
+        <img src="https://br.web.img2.acsta.net/c_310_420/pictures/18/11/12/13/38/3629995.jpg" alt="Supernatural" />
+        <h3>Supernatural</h3>
+      </div>
+
+      <div class="card">
+        <img src="https://br.web.img3.acsta.net/c_310_420/pictures/19/11/26/09/09/5380589.jpg" alt="Anne with an E" />
+        <h3>Anne with an E</h3>
+      </div>
+
+      <div class="card">
+        <img src="https://br.web.img3.acsta.net/c_310_420/pictures/15/03/11/17/20/597260.jpg" alt="Peaky Blinders" />
+        <h3>Peaky Blinders</h3>
+      </div>
+
+      <div class="card">
+        <img src="https://br.web.img3.acsta.net/c_310_420/pictures/17/03/16/16/04/274512.jpg" alt="Prison Break" />
+        <h3>Prison Break</h3>
+      </div>
+    </div>
+    <button class="next-btn">&#10095;</button>
+  </div>
+</section>
 
     <section>
       <h2>Lançamentos</h2>
@@ -110,38 +162,37 @@
           <p>Estreia em 8 de maio de 2025.</p>
         </div>
       </div>
-    </section>
+    </section>  
+  <section>
+  <h2>Filmes Vencedores do Oscar de Melhor Filme</h2>
 
-    <section>
-      <h2>Ganhadores do Oscar</h2>
-      <div class="cards-container">
-        <div class="card-lancamento">
-          <img src="/img/Premonição6.jpg" alt="Filme 1">
-          <h3>Premonição 6: Laços de Sangue</h3>
-          <p>Estreia em 15 de maio de 2025. </p>
-        </div>
-        <div class="card-lancamento">
-          <img src="/img/Manas.jpg" alt="Filme 2">
-          <h3>Manas<br><br></h3>
-          <p>Estreia em 15 de maio de 2025.</p>
-        </div>
-        <div class="card-lancamento">
-          <img src="/img/Final Destination.jpg" alt="Filme 3">
-          <h3>Final Destination: Bloodlines</h3>
-          <p>Estreia em 15 de maio de 2025.</p>
-        </div>
-        <div class="card-lancamento">
-          <img src="/img/Homem com H.jpg" alt="Filme 4">
-          <h3>Homem com H<br><br></h3>
-          <p>Estreia em 1º de maio de 2025.</p>
-        </div>
-        <div class="card-lancamento">
-          <img src="/img/Os enforncados.jpg" alt="Filme 5">
-          <h3>Os Enforcados<br><br></h3>
-          <p>Estreia em 8 de maio de 2025.</p>
-        </div>
-      </div>
-    </section>
+  <div class="cards-container">
+    <div class="card-lancamento">
+      <img src="https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg" alt="Tudo em Todo Lugar ao Mesmo Tempo" />
+      <h3>Tudo em Todo Lugar ao Mesmo Tempo (2023)</h3>
+      <p>Vencedor do Oscar de Melhor Filme em 2023.</p>
+      <p>Data da cerimônia: 12 de março de 2023</p>
+      <p>Um filme emocionante que mistura drama, comédia e fantasia, explorando múltiplas realidades.</p>
+    </div>
+
+    <div class="card-lancamento">
+      <img src="https://upload.wikimedia.org/wikipedia/pt/5/5b/Oppenheimer_poster.jpg" alt="Oppenheimer" />
+      <h3>Oppenheimer (2024)</h3>
+      <p>Vencedor do Oscar de Melhor Filme em 2024.</p>
+      <p>Data da cerimônia: 10 de março de 2024</p>
+      <p>Biografia dramática sobre o físico que liderou o Projeto Manhattan.</p>
+    </div>
+
+    <div class="card-lancamento">
+      <img src="https://i.pinimg.com/736x/fd/0a/78/fd0a7888a1165a085fabf22a1ac3fd41.jpg" alt="imagem com ?" />
+      <h3>Oscar 2025</h3>
+      <p>A cerimônia ainda não ocorreu.</p>
+      <p>Os vencedores serão atualizados após o evento.</p>
+    </div>
+  </div>
+</section>
+
+
 </main>
 
 <footer>
